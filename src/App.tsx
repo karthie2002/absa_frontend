9d44b459319fe0ed6d16d2315cae12f9b2e293b5
Aishwarya <riya129ash@gmail.com>
2023-09-05 14:28:24 +0530

    First!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-  
-     <div className="flex">hi</div>
-   
-  )
+    <div className="flex flex-row">
+      <p>hi</p>
+      <p>hi</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
