28949feb68081d178a237dd61dc76ae8d33faecc
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-09-05 14:19:21 +0530

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return <h1>Customer Review Management</h1>;
+  const [count, setCount] = useState(0)
+
+  return (
+  
+     <div className="flex">hi</div>
+   
+  )
 }
 
-export default App;
+export default App
