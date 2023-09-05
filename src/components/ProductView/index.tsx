2bc7becfbb293c7736946d44356be45799d2bc24
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-09-05 15:10:24 +0530

    d

--- src/components/ProductView/index.tsx
+++ src/components/ProductView/index.tsx
@@ -1,9 +1,13 @@
+import jsonData from '../../../public/products.json'
 const ProductReview = () => {
   return (
     <div className="flex">
-        hi
+      <div className="w-[50%]">
+
+      </div>
+      <div className="w-[50%]">hi</div>
     </div>
-  )
+  );
 };
 
 export default ProductReview;
