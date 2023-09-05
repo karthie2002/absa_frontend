e6b16c65bb3b7c86bee018165f0df9fb84b49d90
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-09-05 15:18:38 +0530

    d

--- src/components/ProductView/index.tsx
+++ src/components/ProductView/index.tsx
@@ -1,9 +1,14 @@
-import jsonData from '../../../public/products.json'
+import * as jsonData from "../../../public/products.json";
 const ProductReview = () => {
+  const prod = jsonData.products;
   return (
     <div className="flex">
       <div className="w-[50%]">
-
+        <div>
+          {prod.map((p) => (
+            <div>{p.product_desc}</div>
+          ))}
+        </div>
       </div>
       <div className="w-[50%]">hi</div>
     </div>
