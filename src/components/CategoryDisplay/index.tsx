9cd25b32cdf206a8ca5d65302f51b3b311f6f441
Aishwarya <riya129ash@gmail.com>
2023-09-17 19:42:49 +0530

    latest

--- src/components/CategoryDisplay/index.tsx
+++ src/components/CategoryDisplay/index.tsx
@@ -1,43 +1,47 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-interface CategoryInterface {
+interface ProductInterface {
   product_id: string;
   product_title: string;
 }
+interface CategoryInterface {
+  product_categry: string;
+}
 
 const CategoryDisplay = () => {
-  const [reviews, setReviews] = useState<CategoryInterface[]>([]);
+  const [products, setProducts] = useState<ProductInterface[]>([]);
+  const [categories, setCategories] = useState<CategoryInterface[]>([]);
 
   useEffect(() => {
     fetch("https://backend-absa.vercel.app/categories/findAllCategories")
       .then((response) => response.json())
       .then((data) => {
-        setReviews(data);
-        console.log(reviews);
+        data = [...data, { product_categry: "HomeDecor" }];
+        setCategories(data);
+        console.log(categories);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    fetch("https://backend-absa.vercel.app/categories/findAllProducts")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        console.log(products);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   }, []);
 
-  // const categories: string[] = [
-  //   "Accent Chairs",
-  //   "Armchairs",
-  //   "Dining Chairs",
-  //   "Office Chairs",
-  //   "Lounge Chairs",
-  //   "Outdoor Chairs",
-  //   "Bean Bag Chairs",
-  //   "Convertible Chairs",
-  // ];
-
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="w-5/6">
         <div className="flex mt-20 bg-[#aeabae] rounded-2xl justify-around">
           <div className="flex flex-col justify-center text-5xl font-semibold">
-            <p>Explore a World of Earphones,</p>
+            <p>Explore a World of Products,</p>
             <p> Crafted For You</p>
           </div>
           <img
@@ -50,22 +54,28 @@ const CategoryDisplay = () => {
         <div className="flex justify-start my-7 gap-9">
           <div className="flex flex-col bg-[#f3f4f6] max-w-[300px] gap-4 h-max py-5 rounded-lg">
             <div className="flex justify-between text-lg bg-[#e7eae8] font-semibold mx-2 px-2 rounded-md">
-              <div>Category</div>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M9.29 15.88L13.17 12L9.29 8.12a.996.996 0 1 1 1.41-1.41l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3a.996.996 0 0 1-1.41 0c-.38-.39-.39-1.03 0-1.42z"
-                  />
-                </svg>
-              </div>
+              {categories.map((item, i) => {
+                return (
+                  <div key={i}>
+                    <div>{item.product_categry}</div>
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M9.29 15.88L13.17 12L9.29 8.12a.996.996 0 1 1 1.41-1.41l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3a.996.996 0 0 1-1.41 0c-.38-.39-.39-1.03 0-1.42z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-            {reviews.map((item, i) => {
+            {products.map((item, i) => {
               return (
                 <div
                   key={i}
@@ -94,7 +104,7 @@ const CategoryDisplay = () => {
           <div className="flex flex-col gap-10 w-full">
             <div className="text-3xl font-semibold">Earphones</div>
             <div className="grid grid-cols-3 grid-flow-row gap-y-10">
-              {reviews.map((item, i) => {
+              {products.map((item, i) => {
                 return (
                   <div key={i} className="w-48">
                     <Link to={`product/${item.product_id}`}>
