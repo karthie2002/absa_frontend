fa9988184ac122f633d3a67bd6341a3e323511e6
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-09-29 18:02:57 +0530

    j

--- src/components/ProductView/index.tsx
+++ src/components/ProductView/index.tsx
@@ -132,7 +132,7 @@ const ProductReview = () => {
     const getOne = await axios.get(
       `https://backend-absa.vercel.app/getOne/${productId}`
     );
-     await axios
+     axios
       .post(
         "https://deef-35-221-227-195.ngrok-free.app/getReview/",
 
